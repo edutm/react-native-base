8cbb5e57e49a83ef4f705a46824eee780a8ffe90
Eduardo <edutm87@hotmail.com>
2021-04-13 15:39:23 -0300

    ajsute btoom bar funcionando

--- lambe/src/navigator.js
+++ lambe/src/navigator.js
@@ -8,9 +8,39 @@ import Icon from 'react-native-vector-icons/FontAwesome'
 import Feed from './screens/Feed'
 
 
-const TabNavigator = createBottomTabNavigator({
-    Home: Feed,
-    Teste: Feed
-});
-  
+const TabNavigator = createBottomTabNavigator(
+    {
+        Feed: {
+            name: 'Feed',
+            screen: Feed,
+            navigationOptions: {
+                title: 'Feed',
+                tabBarIcon: ({ tintColor }) => <Icon name='home' size={30} color={tintColor}/>
+            }
+        },
+        Add: {
+            name: 'Addphoto',
+            screen: Feed,
+            navigationOptions: {
+                title: 'Add Picture',
+                tabBarIcon: ({ tintColor }) => <Icon name='camera' size={30} color={tintColor}/>
+            }
+        },
+        Profile: {
+            name: 'Profile',
+            screen: Feed,
+            navigationOptions: {
+                title: 'Profile',
+                tabBarIcon: ({ tintColor }) => <Icon name='user' size={30} color={tintColor}/>
+            }
+        },
+    },
+    {
+        initialRouteName: 'Feed',
+        tabBarOptions: {
+            showLabel: false
+        }
+    }
+);
+
 export default createAppContainer(TabNavigator);
